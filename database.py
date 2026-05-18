bf133bd2a343809182042e4b7531786db997e9e9
vladykinasofia2007-spec <vladykinasofia2007@gmail.com>
2026-05-19 00:43:15 +0300

    database.py

--- database.py
+++ database.py
@@ -17,6 +17,42 @@ def init_db():
             parity TEXT NOT NULL
         )
     """)
+    #Таблица с ID всех пользователей
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS users (
+            chat_id INTEGER PRIMARY KEY
+        )
+    """)
+    conn.commit()
+    conn.close()
+    #Функция для добавления нового пользователя в базу
+    def add_user(chat_id: int):
+        conn = sqlite3.connect("schedule.db")
+        cursor = conn.cursor()
+        cursor.execute("INSERT OR IGNORE INTO users (chat_id) VALUES (?)",(chat_id,))
+        conn.commit()
+        conn.close()
+
+    def get_all_users():
+        conn = sqlite3.connect("schedule.db")
+        cursor = conn.cursor()
+        cursor.execute("SELECT chat_id FROM users")
+        rows = cursor.fetchall()
+        conn.close()
+        return [row[0] for row in rows]
+
+    def get_schedule_by_day(day: str, current_parity: str):
+        conn = sqlite3.connect("schedule.db")
+        cursor = conn.cursor()
+        cursor.execute("""
+            SELECT pair_time, subject, parity
+            FROM schedule
+            WHERE day_of_week = ? AND (parity = ? OR parity = 'Обе')
+            ORDER BY pair_time
+        """, (day, current_parity))
+        rows = cursor.fetchall()
+        conn.close()
+        return rows        
     
     # Расписание ГУАП с учетом четности недель
     test_data = [
